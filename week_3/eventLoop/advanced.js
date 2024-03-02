47fa6c13a64fb49b984f2993a7b9e4a9cff661f7
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-02 18:09:52 +0800

    ASTONJS-1 [feat]: finished advanced tasks

---
+++ week_3/eventLoop/advanced.js
@@ -0,0 +1,21 @@
+//Продвинутое:
+// Напишите функцию, которая будет проходить через массив целых чисел и выводить индекс
+// каждого элемента с задержкой в 3 секунды.
+
+const arr = [1, 3, 6, 4, 2, 5];
+const delay = 3000;
+
+const printArrWithDelay = (arr, delay = 3000) => {
+  if (arr.length === 0 || delay < 0 || typeof delay !== 'number' || Array.isArray(arr) === false) {
+    console.error('wrong input');
+    return;
+  }
+  arr.forEach((el, i) => {
+    setTimeout(() => {
+      console.log(i);
+    }, delay * i);
+  });
+  console.log('iteration finished');
+};
+printArrWithDelay(arr, delay);
+printArrWithDelay(arr, delay);
