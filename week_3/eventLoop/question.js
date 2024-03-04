3f7973195fb59e03c880fc3a6708277b9788fe8a
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-04 19:19:03 +0800

    ASTONJS-1 [feat]: added question

---
+++ week_3/eventLoop/question.js
@@ -0,0 +1,17 @@
+console.log(1);
+new Promise((reject, resolve) => {
+  reject(2);
+}).then(res => {
+  console.log(res);
+  return Promise.reject(3);
+})
+  .then((res) => {
+    console.log(res);
+    return 4;
+  }, (res) => {
+    console.log(res);
+    return 5;
+  })
+  .catch(err => console.log('error', err))
+  .then(console.log);
+console.log(6);
