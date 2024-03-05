c8c7a70d1d35999638e3ed4a54e2694b05e61272
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-05 20:03:49 +0800

    ASTONJS-1 [feat]: finished advanced task

--- week_4/dom_bom/basic.js
+++ week_4/dom_bom/basic.js
@@ -2,24 +2,24 @@
 //   Написать функцию pattern, принимающую на вход число n и возвращающую в консоли паттерн:
 
 const pattern = (lines) => {
-    let result = '';
-    const addNums = (n) => {
-      for (let k = -0; k < lines - n; k++) result += ' ';
-      for (let j = 0; j < n; j++) result += j + 1;
-      for (let j = 1; j < n; j++) result += n - j;
-    };
-    for (let i = 1; i <= lines; i++) {
-      addNums(i);
-      result += '\n';
-    }
-    for (let i = lines - 1; i > 0; i--) {
-      addNums(i);
-      result += '\n';
-    }
-    console.log(result);
+  if (lines < 1 || typeof lines !== 'number') return null;
+  let result = '';
+  const addNums = (n) => {
+    for (let k = -0; k < lines - n; k++) result += ' ';
+    for (let j = 0; j < n; j++) result += j + 1;
+    for (let j = 1; j < n; j++) result += n - j;
+  };
+  for (let i = 1; i <= lines; i++) {
+    addNums(i);
+    result += '\n';
   }
+  for (let i = lines - 1; i > 0; i--) {
+    addNums(i);
+    result += '\n';
+  }
+  console.log(result);
+};
 pattern(5);
-
 
 
 // function pattern1(n) {
